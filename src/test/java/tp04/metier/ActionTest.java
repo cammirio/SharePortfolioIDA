13ca0287cf79ec429114a1e52e4ea8954605da41
CamilleM <cmirio@hotmail.fr>
2024-01-23 11:36:40 +0100

    Ready

---
+++ src/test/java/tp04/metier/ActionTest.java
@@ -0,0 +1,72 @@
+/*
+ * Copyright 2024 CamilleM.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package tp04.metier;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author CamilleM
+ */
+public class ActionTest {
+    
+    public ActionTest() {
+    }
+    
+    @BeforeAll
+    public static void setUpClass() {
+    }
+    
+    @AfterAll
+    public static void tearDownClass() {
+    }
+    
+    @BeforeEach
+    public void setUp() {
+    }
+    
+    @AfterEach
+    public void tearDown() {
+    }
+
+    /**
+     * Test of getLibelle method, of class Action.
+     */
+    @Test
+    public void testGetLibelle() {
+        
+        // TODO review the generated test code and remove the default call to fail.
+       
+    }
+
+   
+    public class ActionImpl extends Action {
+
+        public ActionImpl() {
+            super("");
+        }
+
+        public float valeur(Jour j) {
+            return 0.0F;
+        }
+    }
+    
+}
