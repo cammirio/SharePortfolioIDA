f5f703a3a35208fcf59297b9560d87c00c48842d
mrgx_pierre <margaux.pierre03@gmail.com>
2024-01-30 10:49:41 +0100

    Correct codesmel [Margaux code - My review]

---
+++ src/main/java/tp/exec/PackageInfo.java
@@ -0,0 +1,13 @@
+/*
+ *Ce package contient la classe permettant lancer une opération sur le portefeuille d'action
+ *(achat, vente, création de portfeuille etc...)
+ */
+package tp.exec;
+
+/**
+ *
+ * @author 33782
+ */
+public class PackageInfo {
+    
+}
---
+++ src/main/java/tp04/metier/PackageInfo.java
@@ -0,0 +1,13 @@
+/*
+ *Ce package contient toutes les classes reliées à la gestion et à la création
+ *d'un portefeuille d'actions simples ou composées.
+ */
+package tp04.metier;
+
+/**
+ *
+ * @author 33782
+ */
+public class PackageInfo {
+    
+}
